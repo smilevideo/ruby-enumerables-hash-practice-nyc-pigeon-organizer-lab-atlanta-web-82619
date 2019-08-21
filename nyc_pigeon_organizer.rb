8ffa288b9600ffb235c3110cb0b38075a259f16a
andrew kim <andrewk1216@gmail.com>
2019-08-21 06:37:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  namelist = 
-  data[data.keys.first].each_key do |key|
+  namelist = []
+  data[data.keys.first].each_value do |key|
     
 end
