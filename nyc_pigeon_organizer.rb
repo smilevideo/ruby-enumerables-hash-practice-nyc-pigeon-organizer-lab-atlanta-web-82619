84fa6e2ceaade7905fb9a89f3a631aa64227408a
andrew kim <andrewk1216@gmail.com>
2019-08-21 06:45:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,7 @@ def nyc_pigeon_organizer(data)
   list = {}
   data.each_value do |attribute|
     attribute.each_value do |value|
-      value.each_with_index
+      value.each_with_index do |name, index|
+        
     
 end
