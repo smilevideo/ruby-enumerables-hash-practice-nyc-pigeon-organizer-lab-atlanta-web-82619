0aacd9027e59262e62775ca5f1c188131f4656b6
andrew kim <andrewk1216@gmail.com>
2019-08-21 06:51:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   list = {}
   data.each_pair do |attribute_key, attribute|
-    attribute.each_value do |value|
+    attribute.each_pair do |value_key, value|
       value.each_with_index do |name, index|
         list[name] = {} if not list[name]
         list[name][attribute]
